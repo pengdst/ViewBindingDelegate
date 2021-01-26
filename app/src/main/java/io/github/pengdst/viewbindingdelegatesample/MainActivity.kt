79068ac6ecac4656974aebaa645076b0e9a4da37
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-01-26 10:42:20 +0700

    Test gradle dependency implementation

--- app/src/main/java/io/github/pengdst/viewbindingdelegatesample/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/viewbindingdelegatesample/MainActivity.kt
@@ -2,10 +2,15 @@ package io.github.pengdst.viewbindingdelegatesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.pengdst.libs.ui.extensions.viewBindings
+import io.github.pengdst.viewbindingdelegatesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBindings()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
